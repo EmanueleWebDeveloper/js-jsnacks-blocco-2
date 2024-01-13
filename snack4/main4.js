1317885130f16d671237c1cbc72cd3a58d096dbf
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-13 18:13:47 +0100

    snack4

--- snack4/main4.js
+++ snack4/main4.js
@@ -2,6 +2,19 @@
 // snack 4
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
+let numero = prompt("Inserisci un numero");
+
+numero = parseInt(numero);
+
+for (let i = 0; i < numero.toString().length; i++) {
+    let cifra = parseInt(numero.toString()[i]);
+
+    if (cifra % 2 === 0) {
+        console.log(cifra);
+    } else {
+        console.log(cifra + 1);
+    }
+}
 
 
 

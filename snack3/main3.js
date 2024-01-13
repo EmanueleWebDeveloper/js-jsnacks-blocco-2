89047e70a93480ca9dddd5c928d5c5a79d1b575f
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-01-13 17:49:41 +0100

    snack 3

--- snack3/main3.js
+++ snack3/main3.js
@@ -1,3 +1,22 @@
 // Ciclo For
 // snack 3
 // Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro
+
+const frutta = ['mela', 'arancia', 'banana'];
+const verdura = ['spinaci', 'broccoli'];
+
+console.log ( frutta, verdura )
+
+for( i = 0; i < frutta.length; i++){
+    
+    const elementi = ['fragole', 'zucchine', 'pere'];
+    
+    if (verdura.length < frutta.length){
+        
+        verdura.push(elementi[i]);
+    }
+
+    console.log(i);
+
+}
+
